77dd8a460c075f93072a12fcc45b12a550e0d0d7
Viet Tien <viettienvu.97@gmail.com>
2024-06-09 19:38:38 +0700

    assignment 02

--- assignments/assignment-02/src/App.jsx
+++ assignments/assignment-02/src/App.jsx
@@ -1,22 +1,44 @@
-import { Routes, BrowserRouter, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import SearchPage from './pages/SearchPage'
+import {
+  // Routes,
+  // BrowserRouter,
+  // Route,
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import {lazy, Suspense} from 'react'
+
+const HomePage = lazy(() => import('./pages/HomePage'))
+const SearchPage = lazy(() => import('./pages/SearchPage'))
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Suspense fallback={<p className='text-light'>Loading ...</p>}>
+        <HomePage />
+      </Suspense>
+    ),
+  },
+  {
+    path: '/search',
+    element: (
+      <Suspense fallback={<p className='text-light'>Loading ...</p>}>
+        <SearchPage />
+      </Suspense>
+    ),
+  },
+])
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* tạo 2 route để chuyển đổi giữa HomePage và SearchPage */}
-        <Route
-          path='/'
-          element={<HomePage />}
-        />
-        <Route
-          path='/search'
-          element={<SearchPage />}
-        />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     {/* tạo 2 route để chuyển đổi giữa HomePage và SearchPage */}
+    //     <Route path='/' element={<HomePage />} />
+    //     <Route path='/search' element={<SearchPage />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
 
